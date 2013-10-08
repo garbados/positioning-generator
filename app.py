cd5b6b6c72bc18e25a0a0c284a31fa34ec48ec29
Max Thayer <garbados@gmail.com>
2013-10-07 20:15:55 -0400

    updated app run

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ app = flask.Flask(__name__)
 def index():
     return flask.render_template('index.html', position = get_position())
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
