a44f5897b11a819cc55b7b057c2028559a39db8c
Max Thayer <garbados@gmail.com>
2013-10-09 14:00:08 -0400

    added text api

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = flask.Flask(__name__)
 def index():
     return flask.render_template('index.html', position = get_position())
 
+@app.route('/api')
+def api():
+    return get_position()
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
